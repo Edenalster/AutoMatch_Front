16e60681633f94e611054fa7577666d60ef5e88a
YoavErel <yoav.e95@gmail.com>
2025-04-02 02:05:37 +0300

    google sign in and register

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+createRoot(document.getElementById("root")!).render(
+  <GoogleOAuthProvider clientId="948364008425-pn2ve5ncji81r9m3c1rk679avbhd3o3e.apps.googleusercontent.com">
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>
+);
